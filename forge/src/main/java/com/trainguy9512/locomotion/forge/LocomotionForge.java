8fe7d5072a0c6910471e89b68bf530c7a65c3750
armandoxtraceroespalda-dev <armandoxtraceroespalda@gmail.com>
2026-03-22 20:01:44 -0700

    Config screen forge

--- forge/src/main/java/com/trainguy9512/locomotion/forge/LocomotionForge.java
+++ forge/src/main/java/com/trainguy9512/locomotion/forge/LocomotionForge.java
@@ -1,11 +1,25 @@
 package com.trainguy9512.locomotion.forge;
 
 import com.trainguy9512.locomotion.LocomotionMain;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(LocomotionMain.MOD_ID)
 public class LocomotionForge {
     public LocomotionForge() {
         LocomotionMain.initialize();
+
+        if (FMLEnvironment.dist.isClient()) {
+            ModLoadingContext.get().registerExtensionPoint(
+                    ConfigScreenHandler.ConfigScreenFactory.class,
+                    () -> new ConfigScreenHandler.ConfigScreenFactory(
+                            (minecraft, parent) -> LocomotionMain.CONFIG.getConfigScreen(
+                                    modId -> net.minecraftforge.fml.ModList.get().isLoaded(modId)
+                            ).apply(parent)
+                    )
+            );
+        }
     }
 }
